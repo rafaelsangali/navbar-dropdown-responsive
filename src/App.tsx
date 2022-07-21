7aea0dbc224679f1a728551ba8d2db40a5f0f9b8
Rafael Sangali <rafaelsangali@outlook.com>
2022-07-21 00:18:57 -0300

    alterando conforme a ideia

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import Navbar from "./components/Navbar/Navbar"
-import {BrowserRouter , Routes, Route} from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-   
     <BrowserRouter>
-    <Routes>
-    
-    <Route path="/" element={<Navbar />} />
-
-    
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+      </Routes>
     </BrowserRouter>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,45 +1,43 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import NavLinks from "./NavLinks";
+import { linksNav } from "./mylinks";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
   return (
     <>
-    <nav className="bg-white fixed w-screen">
-      <div className="flex w-auto items-center font-medium justify-around">
-        <div className="z-50 p-5 md:w-auto w-full flex justify-between">
+      <header className="bg-white w-screen shadow-md flex items-center font-medium justify-around">
+        <div className="p-5 md:w-auto w-full flex justify-between">
           <img src={""} alt="logo" className="md:cursor-pointer h-9" />
           <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
             <button>{open ? "fechar" : "abrir"}</button>
           </div>
         </div>
-        <ul className="md:flex hidden uppercase items-center gap-8">
-          <li>
-            <Link to="/" className="py-7 px-3 inline-block">
-              Home
-            </Link>
-          </li>
-          <NavLinks />
-        </ul>
-
-        {/* Mobile nav */}
-        <ul
-          className={`
-        md:hidden bg-white fixed w-3/5 top-0 overflow-y-auto bottom-0 py-24 pl-4
-        duration-500 ${open ? "left-0" : "left-[-100%]"}
-        `}
-        >
-          <li>
-            <Link to="/" className="py-7 px-3 inline-block">
-              Home
-            </Link>
-          </li>
-          <NavLinks />
-        </ul>
-      </div>
-    </nav>
-    <div className="h-[1000px] bg-slate-600"></div>
+        <nav>
+          <ul className="md:flex hidden items-center gap-8">
+            {linksNav.map((link, index) => (
+              <li key={link.name + index}>
+                <Link to={link.redirect} className="px-3 inline-block hover:text-green-400 transition-colors">
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul
+            className={`md:hidden bg-zinc-500 fixed w-3/5 top-0 overflow-y-auto bottom-0 py-24 pl-4 duration-500 ${
+              open ? "left-0" : "left-[-100%]"
+            }`}
+          >
+            {linksNav.map((link, index) => (
+              <li key={link.name + index}>
+                <Link to={link.redirect} className="px-3 inline-block hover:text-green-400 transition-colors">
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
